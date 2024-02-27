8a887e64d8d4251f1104077d86ea7e253ab5e2f7
Bastien Cherel <bastien@MacBook-Pro-de-Bastien.local>
2024-02-27 15:55:40 +0100

    Answered to part 1 and 2

--- src/onionRouters/simpleOnionRouter.ts
+++ src/onionRouters/simpleOnionRouter.ts
@@ -8,8 +8,22 @@ export async function simpleOnionRouter(nodeId: number) {
   onionRouter.use(bodyParser.json());
 
   // TODO implement the status route
-  // onionRouter.get("/status", (req, res) => {});
+  onionRouter.get("/status", (req, res) => {res.send("live");});
 
+  let lastReceivedEncryptedMessage: string | null = null;
+  let lastMessageDestination: number | null = null;
+
+  onionRouter.get("/getLastReceivedEncryptedMessage", (req, res) => {res.json({ result: lastReceivedEncryptedMessage })});
+  
+  onionRouter.get("/getLastReceivedDecryptedMessage", (req , res) => {res.json({ result: lastReceivedEncryptedMessage })});
+  
+  onionRouter.get("/getLastMessageDestination", (req, res) => {res.json({ result: lastMessageDestination })});
+ 
+  
+  
+  
+  
+  
   const server = onionRouter.listen(BASE_ONION_ROUTER_PORT + nodeId, () => {
     console.log(
       `Onion router ${nodeId} is listening on port ${
